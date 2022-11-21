211748dd4feea8b9b5e24c5aa448fff0a106ac17
Antoine Zen-Ruffinen <antoine.zen@gmail.com>
2022-11-21 21:43:30 +0100

    Build basic xtask commands.

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,23 @@
+use clap::{command, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let mut cmd = command!()
+        .subcommand(Command::new("get-apikey").about("Get test server APIKEY."))
+        .subcommand(Command::new("start-server").about("Start test server"))
+        .subcommand(Command::new("shutdown-server").about("Shutdown test server"));
+
+    match cmd.get_matches_mut().subcommand() {
+        Some(("get-apikey", _)) => {
+            println!("get-apikey");
+        }
+        Some(("start-server", _)) => {
+            println!("start-server");
+        }
+        Some(("shutdown-server", _)) => {
+            println!("shutdown!");
+        }
+        _ => {
+            cmd.print_help().unwrap();
+        }
+    }
 }
