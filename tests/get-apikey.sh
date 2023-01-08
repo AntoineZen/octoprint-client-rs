f17f04247934f67249b00e4f5bf62e7be9aa4cb7
Antoine Zen-Ruffinen <antoine.zen@gmail.com>
2023-01-08 22:19:57 +0100

    Reverted to old "get-apikey.sh".
    As it seems that calling cargo from cargo brings some error...

--- tests/get-apikey.sh
+++ tests/get-apikey.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-cargo xtask get-apikey
+DIR=$(dirname "$0")
+
+DATA_ARCHIVE="$DIR/data.zip"
+
+#unzip -p $DIR/data.zip basedir/config.yaml | yq .api.key
+unzip -p $DATA_ARCHIVE basedir/users.yaml | yq .rust.apikey
